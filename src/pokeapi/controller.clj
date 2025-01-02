fd33fd39f3742a5cf69b35c4f9d9ddf9c5b35f24
josue santos <josue.santos.dev@gmail.com>
2025-01-02 18:11:59 -0300

    feature(controller): implement controller to return an pokemon

---
+++ src/pokeapi/controller.clj
@@ -0,0 +1,14 @@
+(ns pokeapi.controller
+  (:require [pokeapi.integration]
+            [pokeapi.adapter]))
+
+(defn get-pokemon
+  [{{:keys [pokemon]} :path-params}]
+  (try
+    {:body (-> (pokeapi.integration/get-pokemon pokemon)
+               (pokeapi.adapter/data->payload))
+     :status 200}
+    (catch Exception e
+      (let [{:keys [body status]} (ex-data e)]
+        {:body   {:message body}
+         :status status}))))
--- src/pokeapi/core.cljs
+++ src/pokeapi/core.cljs
@@ -4,6 +4,7 @@
    [muuntaja.core :as m]
    [reitit.coercion.malli]
    [reitit.ring :as ring]
+   [pokeapi.controller]
    [reitit.ring.middleware.muuntaja :as muuntaja]
    [ring.adapter.jetty :as ring.jetty]))
 
@@ -12,12 +13,14 @@
 (def app
   (ring/ring-handler
    (ring/router
-    ["/ping" {:handler (fn [_] {:body "pong" :status 200})}]
+    ["/api" 
+     ["/ping" {:handler (fn [_] {:body "pong" :status 200})}]
+     ["/pokemon/:pokemon" {:get {:handler #(pokeapi.controller/get-pokemon %)}}]]
     {:data {:muuntaja m/instance
             :middleware [muuntaja/format-middleware]}})))
 
 (defn start []
-  (ring.jetty/run-jetty #'app {:port  3030
+  (ring.jetty/run-jetty #'app {:port  3000
                                :join? false}))
 
 (defn -main
