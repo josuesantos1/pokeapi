f48fde09f8fde3e9afd86dcfb3104bebf252e28c
josue santos <josue.santos.dev@gmail.com>
2025-01-02 18:26:38 -0300

    refactor(integration): improving integration impl

--- src/pokeapi/integration.clj
+++ src/pokeapi/integration.clj
@@ -2,9 +2,9 @@
   (:require [clj-http.client :as client]
             [clojure.data.json :as json]))
 
-(def pokeapi-base-url "https://pokeapi.co/api/v2/")
+(def ^:private pokeapi-base-url "https://pokeapi.co/api/v2/")
 
-(defn get-request [url]
+(defn- get-request [url]
   (-> (client/get (str pokeapi-base-url url))
       :body
       json/read-str))
