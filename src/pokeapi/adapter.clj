f9f36139ee80a2c0094d9b2fb9ecd6fbafba4dc7
josue santos <josue.santos.dev@gmail.com>
2025-01-02 17:23:50 -0300

    feature(adapter): implement an adapter to adapt the response data to the payload

---
+++ src/pokeapi/adapter.clj
@@ -0,0 +1,11 @@
+(ns pokeapi.adapter)
+
+(defn- get-ability 
+  [pokemon]
+  (->> (get pokemon "abilities")
+       (map #(get-in % ["ability" "name"]))
+       sort))
+
+(defn data->payload [pokemon]
+  (let [abilities (get-ability pokemon)]
+    {:abilities abilities}))
