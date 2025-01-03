fc1a26f3b7b98627c9c3c825288797857f2ca2c3
josue santos <josue.santos.dev@gmail.com>
2025-01-03 13:27:00 -0300

    feature(pokeapi): added a route to assets and index
    routes:
    /public/assets => assets
    / => index

--- src/clj/pokeapi/core.clj
+++ src/clj/pokeapi/core.clj
@@ -1,6 +1,7 @@
 (ns pokeapi.core
   (:gen-class)
   (:require
+   [clojure.java.io :as io]
    [muuntaja.core :as m]
    [pokeapi.controller]
    [reitit.coercion.malli]
@@ -10,12 +11,19 @@
 
 (defonce server (atom nil))
 
+(defn index []
+  (slurp (io/resource "public/index.html")))
+
+
 (def app
   (ring/ring-handler
    (ring/router
-    ["/api"
-     ["/ping" {:handler (fn [_] {:body "pong" :status 200})}]
-     ["/pokemon/:pokemon" {:get {:handler #(pokeapi.controller/get-pokemon %)}}]]
+    ["/"
+     ["/api"
+      ["/ping" {:handler (fn [_] {:body "pong" :status 200})}]
+      ["/pokemon/:pokemon" {:get {:handler #(pokeapi.controller/get-pokemon %)}}]]
+     ["assets/*" (ring/create-resource-handler {:root "public/assets"})]
+     ["" {:handler (fn [_] {:body (index) :status 200})}]]
     {:data {:muuntaja m/instance
             :middleware [muuntaja/format-middleware]}})))
 
