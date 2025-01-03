7815d20f5c065223d2793818f162b198bdb655b0
josue santos <josue.santos.dev@gmail.com>
2025-01-03 16:39:56 -0300

    feature(adapter): added fields with data
    fields:
    sprite
    stats
    weight
    height
    name

--- src/clj/pokeapi/adapter.clj
+++ src/clj/pokeapi/adapter.clj
@@ -6,6 +6,23 @@
        (map #(get-in % ["ability" "name"]))
        sort))
 
+(defn- get-sprites 
+  [pokemon]
+  (get-in pokemon ["sprites" "front_shiny"]))
+
+(defn- get-stats 
+  [pokemon]
+  (->>
+   (get pokemon "stats")
+   (map (fn [stat]
+          {:name (get-in stat ["stat" "name"])
+           :base-stat (get stat "base_stat")
+           :effort (get stat "effort")}))))
+
 (defn data->payload [pokemon]
-  (let [abilities (get-ability pokemon)]
-    {:abilities abilities}))
+  {:abilities (get-ability pokemon)
+   :sprite (get-sprites pokemon)
+   :stats (get-stats pokemon)
+   :weight (get pokemon "weight")
+   :height (get pokemon "height")
+   :name (get pokemon "name")})
