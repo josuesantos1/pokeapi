b2e04ce4bf70c717afebdae28a0fa2ff38940de3
josue santos <josue.santos.dev@gmail.com>
2025-01-11 12:03:27 -0300

    style(view): improve style from input
    added conditional render in stats and abilities

--- src/cljs/app/view.cljs
+++ src/cljs/app/view.cljs
@@ -18,27 +18,30 @@
            (d/div {:class "flex flex-col items-center justify-between p-5 bg-gray-50 rounded-lg shadow-md"}
             (d/div
              (d/input {:placeholder "Buscar pokemon"
-                       :class-name "form-control border border-solid border-gray-600"
+                       :class-name "border border-gray-300 rounded px-4 py-2"
                        :on-change #(set-state assoc :pokemon (.. % -target -value))})
-             (d/button {:onClick #(fetch-slug)} "Buscar"))
+             (d/button {:class "px-4 py-2 bg-gray-500 hover:bg-gray-300 text-white hover:text-gray-500 rounded"
+                        :onClick #(fetch-slug)} "Buscar"))
             (some->> (:name response) (d/h1 {:class "text-5xl m-5"}))
             (some->> (:weight response) (d/strong "weight: "))
             (d/img {:src (:sprite response)
                     :class "w-1/5 h-auto rounded-lg shadow-lg"}))
-           (d/div {:class "flex flex-col space-y-4 p-4 bg-gray-100 rounded shadow-lg"}
-                  (d/strong "Abilities")
-                  (map
-                   (fn [item]
-                     (d/div {:key item
-                             :class "flex items-center justify-between p-5 bg-white rounded-lg shadow-md hover:bg-gray-50"}
-                            (d/span {:class "text-gray-700 font-medium"} item)))
-                   (:abilities response)))
-           (d/div {:class "flex flex-col space-y-4 p-4 bg-gray-100 rounded shadow-lg"}
-                  (d/strong "stats")
-                  (map
-                   (fn [item]
-                     (d/div {:key item
-                             :class "flex items-center justify-between p-5 bg-white rounded-lg shadow-md hover:bg-gray-50"}
-                            (d/span {:class "text-gray-700 font-medium"} (:name item))
-                            (d/span {:class "text-gray-700 font-medium"} (:base-stat item))))
-                   (:stats response))))))
+           (when (seq response)
+             (d/div 
+              (d/div {:class "flex flex-col space-y-4 p-4 m-3 bg-gray-100 rounded shadow-lg"}
+                     (d/strong {:class "text-lg"} "Abilities")
+                     (map
+                      (fn [item]
+                        (d/div {:key item
+                                :class "flex items-center justify-between p-5 bg-white rounded-lg shadow-md hover:bg-gray-50"}
+                               (d/span {:class "text-gray-700 font-medium"} item)))
+                      (:abilities response)))
+              (d/div {:class "flex flex-col space-y-4 p-4 m-3 bg-gray-100 rounded shadow-lg"}
+                     (d/strong {:class "text-lg"} "Stats")
+                     (map
+                      (fn [item]
+                        (d/div {:key item
+                                :class "flex items-center justify-between p-5 bg-white rounded-lg shadow-md hover:bg-gray-50"}
+                               (d/span {:class "text-gray-700 font-medium"} (:name item))
+                               (d/span {:class "text-gray-700 font-medium"} (:base-stat item))))
+                      (:stats response))))))))
